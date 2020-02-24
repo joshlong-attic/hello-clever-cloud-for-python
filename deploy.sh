de7834edd8c54b50c27c29688ff62f81d6aa46b1
Josh Long <josh@joshlong.com>
2020-02-23 16:52:44 -0800

    polish

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 # convert the Pipenv to requirements.txt
 ## TODO set the CLEVER env variables
-pipenv lock -r > requirements.txt
+pipenv lock -r >requirements.txt
+clever env set CC_PYTHON_MODULE main
+clever env set CC_PYTHON_VERSION 3.7
 clever deploy
