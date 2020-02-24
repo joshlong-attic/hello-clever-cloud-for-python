16e7e7a892bdde8118d3471baff58987189e4534
Josh Long <josh@joshlong.com>
2020-02-23 17:01:33 -0800

    its dirty

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 # convert the Pipenv to requirements.txt
 ## TODO set the CLEVER env variables
+echo "1" >> dirty
+git add dirty
+git commit -am "its dirty"
 pipenv lock -r > requirements.txt
-clever env set CC_PYTHON_MODULE main:main
+clever env set CC_PYTHON_MODULE main:app
 clever env set CC_PYTHON_VERSION 3.7
 #clever deploy
-clever restart
+clever deploy -f
